cbbab7234fc6bda9a1c4afe99679e5e526794f84
Hossam7192 <hossamali7192@gmail.com>
2026-04-03 23:42:20 +0300

    final update portfolio

--- Lecture-04-responsive/script.js
+++ Lecture-04-responsive/script.js
@@ -6,10 +6,24 @@ let isDarkMode = false;
 function toggleTheme() {
   isDarkMode = !isDarkMode;
   document.body.classList.toggle("dark-mode");
+  localStorage.setItem("portfolio_theme", isDarkMode);
   console.log("Theme changed:", isDarkMode);
 }
 
 function countClick() {
   clickCount++;
+  document.getElementById("clickCountText").textContent = "Clicks: " + clickCount;
   console.log("Button clicked:", clickCount);
 }
+
+window.onload = function () {
+  const savedTheme = localStorage.getItem("portfolio_theme");
+
+  if (savedTheme === "true") {
+    document.body.classList.add("dark-mode");
+    isDarkMode = true;
+  }
+
+  const today = new Date().toISOString().split("T")[0];
+  document.getElementById("lastUpdated").textContent = "Last updated: " + today;
+};
